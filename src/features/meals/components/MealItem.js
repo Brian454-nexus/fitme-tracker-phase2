1eed4f2ab705efa447bd66ce8b9488bb6463b9cd
Andrew Tobiko <andrew.tobiko1@student.moringaschool.com>
2025-04-22 14:50:42 +0300

    Update MealItem to display meal details

--- src/features/meals/components/MealItem.js
+++ src/features/meals/components/MealItem.js
@@ -1,1 +1,1 @@
-import React from "react"; const MealItem = () => { return ( <div className="meal-item"> <h4>Meal Name</h4> </div> ); }; export default MealItem;
+import React from "react"; const MealItem = ({ meal }) => { return ( <div className="meal-item"> <h4>{meal.name}</h4> <p>{meal.calories} calories</p> </div> ); }; export default MealItem;
