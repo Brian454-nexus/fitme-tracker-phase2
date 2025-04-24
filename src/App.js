6e11432207de23455127aee0f9abfa5e699272f6
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-04-24 12:57:37 +0300

    feat: add navigation menu between features

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import WorkoutGenerator from "./components/workout/WorkoutGenerator";
 import WaterIntakeTracker from "./components/hydration/WaterIntakeTracker";
 import { ThemeProvider as CustomThemeProvider } from "./context/ThemeContext";
 import { lightTheme } from "./theme";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <CustomThemeProvider>
       <ThemeProvider theme={lightTheme}>
         <BrowserRouter>
+          <Navigation />
           <Routes>
             <Route path="/" element={<WorkoutGenerator />} />
             <Route path="/hydration" element={<WaterIntakeTracker />} />
