781c0a827e07bf83e8101f4566979b024ec30ade
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-04-24 12:55:34 +0300

    fix: update react-router-dom imports and version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WorkoutGenerator from "./components/workout/WorkoutGenerator";
 import WaterIntakeTracker from "./components/hydration/WaterIntakeTracker";
 import { ThemeProvider as CustomThemeProvider } from "./context/ThemeContext";
-import { lightTheme } from './theme';
+import { lightTheme } from "./theme";
 
 function App() {
   return (
     <CustomThemeProvider>
       <ThemeProvider theme={lightTheme}>
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<WorkoutGenerator />} />
             <Route path="/hydration" element={<WaterIntakeTracker />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </ThemeProvider>
     </CustomThemeProvider>
   );
